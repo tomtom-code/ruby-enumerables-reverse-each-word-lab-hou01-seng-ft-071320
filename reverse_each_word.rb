b1a364f08242d463ad47e77071fd03501917f571
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-07-04 17:46:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,14 @@ require 'pry'
 
 def reverse_each_word(sentence)
 
-new_sentence =[ ]
+ new_sentence =[ ]
 
-array = sentence.split
+ array = sentence.split
 
  array.collect do |element| 
   new_sentence << element.reverse
  end
 
-new_sentence.join("")
+ new_sentence.join("")
 
 end
